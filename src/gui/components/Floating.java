ab21e94975a1c7eeffc002994fd74f1dd528d8f0
HonzaBoh <honza.boh@gmail.com>
2024-09-30 14:09:38 +0200

    textfields

--- src/gui/components/Floating.java
+++ src/gui/components/Floating.java
@@ -8,17 +8,14 @@ public class Floating extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setSize(700, 700);
-        this.setLayout(null);
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.gray);
+        //JPanel panel = new JPanel();
+        this.setBackground(Color.gray);
 
-        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        panel.setBounds(250, 250, 300, 300);
+        this.setLayout(new FlowLayout(FlowLayout.TRAILING, 10, 10));
 
         for (int i = 0; i < 10; i++) {
-            panel.add(new JButton(String.valueOf(i)));
+            this.add(new JButton(String.valueOf(i)));
         }
-        this.add(panel);
     }
 
     public static void main(String[] args) {
