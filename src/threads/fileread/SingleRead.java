46b5787dae70f758c0e637816ee6b3e38dd9e2a0
bohac <honza.boh00@gmail.com>
2024-12-13 11:51:31 +0100

    Threads basics, sync

--- src/threads/fileread/SingleRead.java
+++ src/threads/fileread/SingleRead.java
@@ -1,30 +1,32 @@
 package threads.fileread;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class SingleRead {
 
     public static void main(String[] args) throws InterruptedException {
 
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 10; i++) { //single thread varianta, vytvori, seradi, jeden po jednom
-            ReaderThread singleRead = new ReaderThread("num"+i+".txt");
-            singleRead.doStuff();
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < 50; i++) {
+                String fileName = "num" + i + ".txt";
+                try {
+                    List<String> lines = Files.readAllLines(Paths.get(fileName));
+                    int[] nums = new int[lines.size()];
+                    for (int j = 0; j < lines.size(); j++) {
+                        nums[j] = Integer.parseInt(lines.get(j));
+                    }
+                    Arrays.sort(nums);
+                    System.out.println("Soubor: " + fileName);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("Single-thread duration: " + (endTime - startTime) + " ms");
         }
-        long stop = System.currentTimeMillis();
-        System.out.println("Duration (single): " + (stop-start));
-
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 5; i++) {
-            ReaderThread singleRead = new ReaderThread("num"+i+".txt");
-            singleRead.doStuff();
-        }
-        stop = System.currentTimeMillis();
-        System.out.println("Duration (single): " + (stop-start));
-
-//        for (int i = 0; i < 10; i++) { // multithread
-//            ReaderThread singleRead = new ReaderThread("num"+i+".txt");
-//            singleRead.start();
-//            singleRead.join();
-//        }
-
-    }
 }
